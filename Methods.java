eb23fccc8c1bfeb6c74f52a540f2815dc6f9de69
Ashutosh Kumar <ashutoshkr0195@gmail.com>
2025-01-13 01:07:56 +0530

    my first method program

---
+++ Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+
+    public static void main(String[] args) {
+        System.out.println("creating first methos program");
+
+    }
+
+    // create method
+    public static void MyName() {
+        System.out.println("my name is Ashutosh Shukla");
+    }
+}
