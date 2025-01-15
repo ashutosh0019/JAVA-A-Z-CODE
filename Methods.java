b1cd6e311c573a850199cd7cf961c80ad63e499d
Ashutosh Kumar <ashutoshkr0195@gmail.com>
2025-01-16 04:20:26 +0530

    method with arguments

--- Methods.java
+++ Methods.java
@@ -1,12 +1,25 @@
 public class Methods {
 
     public static void main(String[] args) {
-        System.out.println("creating first methos program");
+        System.out.println("creating methods or function program");
+
+        // calling methods
+        //static method (without user input)
+        MyName();
+
+        //dynamic method (with arguments)
+        stu_name("ashutosh", 1);
 
     }
 
     // create method
     public static void MyName() {
         System.out.println("my name is Ashutosh Shukla");
+    }
+
+    //methods with arguments
+    public static void stu_name(String name, int roll){
+        System.out.println("name: "+ name);
+        System.out.println("roll: "+ roll);
     }
 }
