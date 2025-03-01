eefb99eb8a44145284b452cb1f73c49dec35322a
Ashutosh Kumar <ashutoshkr0195@gmail.com>
2025-03-01 17:00:46 +0000

    update some code

--- ArrayBasic_12.java
+++ ArrayBasic_12.java
@@ -7,6 +7,7 @@ public class ArrayBasic_12 {
         int arr[] = {1,2,2,3,4,1,3,6,4,5,6,7,7,8,9};
 
         Arrays.sort(arr);
+        System.out.println("the length of an array is "+arr.length);
         System.out.print("Array after removing duplicates: ");
         System.out.print(arr[0] + " ");
 
@@ -15,5 +16,8 @@ public class ArrayBasic_12 {
                 System.out.print(arr[i]+" ");
             }
         }
+        System.out.println();
+
+        System.out.println("here the length of an array is still "+arr.length+", because here we remove only duplicates value not remove the blank space of array");
     }
 }
